2b7db9f2344451ee29f7ad1d67d221b82a4a9468
mahdibaderloo <mahdi.baderloo.230@gmail.com>
2025-04-16 20:40:49 +0330

    import items from db.js

--- Scripts/dashboard.js
+++ Scripts/dashboard.js
@@ -1,3 +1,22 @@
+import {
+  sadRobosta,
+  vaftad30Robosta,
+  vaftad30Arabica,
+  sadArabica,
+  hot,
+  coldCoffee_100r,
+  coldCoffee_7030r,
+  coldCoffee_7030a,
+  coldCoffee_100a,
+  shake,
+  cold,
+  cake,
+  snack,
+  tea_1,
+  tea_2,
+  bubbleTea,
+} from "../Scripts/db.js";
+
 const headerTitle = document.querySelector(".header-title");
 const menu = document.querySelector(".menu-section");
 const menuIcon = document.querySelector(".menu-icon");
@@ -5,6 +24,7 @@ const menuItem = document.querySelectorAll(".menu-item");
 const dashboardSection = document.querySelector(".dashboard-section");
 const productsSection = document.querySelector(".products-section");
 const profileSection = document.querySelector(".profile-section");
+const categories = document.querySelectorAll(".category");
 
 menuIcon.addEventListener("click", () => {
   menu.style.right = "0";
@@ -33,6 +53,12 @@ menuItem.forEach((item) => {
         headerTitle.innerHTML = "پروفایل";
         break;
     }
+  });
+});
+
+categories.forEach((category) => {
+  category.addEventListener("click", (e) => {
+    console.log(e.target.dataset.item);
   });
 });
 
