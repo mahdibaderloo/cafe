5821c63b2058738717d9defd38991a683758122f
mahdibaderloo <mahdi.baderloo.230@gmail.com>
2025-04-16 18:26:52 +0330

    dashboard menu buttons

--- Scripts/dashboard.js
+++ Scripts/dashboard.js
@@ -1,8 +1,35 @@
 const menu = document.querySelector(".menu-section");
 const menuIcon = document.querySelector(".menu-icon");
+const menuItem = document.querySelectorAll(".menu-item");
+const dashboardSection = document.querySelector(".dashboard-section");
+const productsSection = document.querySelector(".products-section");
+const profileSection = document.querySelector(".profile-section");
 
 menuIcon.addEventListener("click", () => {
   menu.style.right = "0";
+});
+
+menuItem.forEach((item) => {
+  item.addEventListener("click", (e) => {
+    console.log(e.target.parentElement.id);
+    switch (e.target.parentElement.id) {
+      case "dashboard-btn":
+        dashboardSection.style.display = "flex";
+        productsSection.style.display = "none";
+        profileSection.style.display = "none";
+        break;
+      case "products-btn":
+        dashboardSection.style.display = "none";
+        productsSection.style.display = "flex";
+        profileSection.style.display = "none";
+        break;
+      case "profile-btn":
+        dashboardSection.style.display = "none";
+        productsSection.style.display = "none";
+        profileSection.style.display = "flex";
+        break;
+    }
+  });
 });
 
 window.addEventListener("click", (e) => {
