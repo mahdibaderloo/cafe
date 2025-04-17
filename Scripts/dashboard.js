4c890db621a73b2aca315d7691548a618cf6abbf
mahdibaderloo <mahdi.baderloo.230@gmail.com>
2025-04-17 15:10:28 +0330

    generateItems function

--- Scripts/dashboard.js
+++ Scripts/dashboard.js
@@ -26,6 +26,7 @@ const CategoriesSection = document.querySelector(".categories-section");
 const profileSection = document.querySelector(".profile-section");
 const categories = document.querySelectorAll(".category");
 const ProductsSection = document.querySelector(".products-section");
+const ProductsWrapper = document.querySelector(".products");
 
 menuIcon.addEventListener("click", () => {
   menu.style.right = "0";
@@ -59,12 +60,33 @@ menuItem.forEach((item) => {
 
 categories.forEach((category) => {
   category.addEventListener("click", (e) => {
-    console.log(e.target);
-    // switch (e.target.dataset.item) {
-    //   case "cold-drink":
-    // }
+    switch (e.target.dataset.item) {
+      case "cold-drink":
+        CategoriesSection.style.display = "none";
+        ProductsSection.style.display = "block";
+        generateItems(cold);
+    }
   });
 });
+
+function generateItems(items) {
+  items.forEach((item) => {
+    ProductsWrapper.insertAdjacentHTML(
+      "beforeend",
+      `<li class="product">
+            <img
+              src="Images/Cafe-Au-Lait-001s.jpg"
+              alt="pic"
+              class="product-img"
+            />
+            <div>
+              <p class="product-title">${item.product}</p>
+              <p class="product-price">${item.price.toLocaleString()}</p>
+            </div>
+          </li>`
+    );
+  });
+}
 
 function productsElement() {}
 
